9ad7f9d8c2fdafe1368bd9b19880845175affddf
Administrator <Administrator@PC-20171102NTAS>
2019-05-10 17:33:42 +0800

    1、增加数据不一致断言 2、增加记录总数测试用例，未完待续

---
+++ src/test/java/com/contract/web/cases2/TotalnumberOfRecords.java
@@ -0,0 +1,44 @@
+package com.contract.web.cases2;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.contract.web.cases.BaseElectron;
+import com.contract.web.util.AssertionUtil;
+import com.contract.web.util.ExcelUtil;
+
+public class TotalnumberOfRecords extends BaseElectron {
+	@Test(priority = 0, dataProvider = "pageNames")
+	public void successCase(String menuname, String pagename) throws Exception {
+		click(getElement("首页页", menuname));
+		click(getElement("进货页", pagename));
+		Thread.sleep(3000);
+		// 创建动作对象，并且指定操作的浏览器
+		WebElement serialNumber = getElement("进货页", "序号1");
+		// 鼠标操作，用到actions方法
+		Actions actions = new Actions(driver);
+		// actions.doubleClick(serialNumber).perform();
+		// 右键序号1
+		actions.contextClick(serialNumber).perform();
+		Thread.sleep(2000);
+		String totalnumberOfRecords = getText(getElement("进货页", "总记录数"));
+		// 断言总记录数是否等于0，等于0即为失败
+		AssertionUtil.assertTextnotEquals(totalnumberOfRecords, "总记录数:0");
+		Thread.sleep(1000);
+		// 鼠标需要移动到页面标题，否则关闭按钮不显示，导致无法点击
+		actions.moveToElement(getElement("首页页", pagename)).perform();
+		Thread.sleep(1000);
+		click(getElement("首页页", "关闭页面"));
+		Thread.sleep(1000);
+	}
+
+	@DataProvider(name = "pageNames")
+	public Object[][] pageNameDatas() {
+		String[] cellNames = { "菜单名称", "菜单页面名称" };
+		Object[][] datas = ExcelUtil.read2("src/test/resources/menuName.xlsx", "DL-1", cellNames);
+		return datas;
+
+	}
+}
--- src/test/java/com/contract/web/util/AssertionUtil.java
+++ src/test/java/com/contract/web/util/AssertionUtil.java
@@ -3,48 +3,69 @@ package com.contract.web.util;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
 public class AssertionUtil {
 	private static Logger logger = Logger.getLogger(AssertionUtil.class);
-	/**断言两者一致情况
+
+	/**
+	 * 断言两者一致情况
+	 * 
 	 * @param actual
 	 * @param expected
 	 */
-	public static void assertTextEquals(String actual,String expected){
-		logger.info("断言比较两者的值是否一致，实际值为：【"+actual+"】，期望值为：【"+expected+"】");
-		Assert.assertEquals(expected, actual,"两者不一致");
+	public static void assertTextEquals(String actual, String expected) {
+		logger.info("断言比较两者的值是否一致，实际值为：【" + actual + "】，期望值为：【" + expected + "】");
+		Assert.assertEquals(expected, actual, "两者不一致");
 	}
-	/*断言失败后继续执行
+
+	/**
+	 * 断言两者不一致情况
+	 * 
+	 * @param actual
+	 * @param expected
+	 */
+	public static void assertTextnotEquals(String actual, String expected) {
+		logger.info("断言比较两者的值是否不一致，实际值为：【" + actual + "】，对比值为：【" + expected + "】");
+		Assert.assertNotEquals(expected, actual, "两者一致");
+	}
+
+	/*
+	 * 断言失败后继续执行
 	 * 
 	 * 
-	 * */
-	public static void assertTextEqualsNoInterruption(String actual,String expected){
-		logger.info("断言比较两者的值是否一致，实际值为：【"+actual+"】，期望值为：【"+expected+"】");
-		 Assertion.verifyEquals(expected, actual,"两者不一致");
+	 */
+	public static void assertTextEqualsNoInterruption(String actual, String expected) {
+		logger.info("断言比较两者的值是否一致，实际值为：【" + actual + "】，期望值为：【" + expected + "】");
+		Assertion.verifyEquals(expected, actual, "两者不一致");
 	}
-	/**断言为真的情况
+
+	/**
+	 * 断言为真的情况
+	 * 
 	 * @param actual
 	 */
-	public static void assertTrue(boolean actual){
-		logger.info("断言元素【"+actual+"】是否在页面显示");
+	public static void assertTrue(boolean actual) {
+		logger.info("断言元素【" + actual + "】是否在页面显示");
 		Assert.assertTrue(actual);
 	}
-	public static void assertE(){
-		
-	}
-	/*判断元素是否存在
-	 * 
-	 * */
-	public static boolean ElementExist(WebDriver driver,By webElement){
-		logger.info("判断元素【"+webElement+"】是否存在");
-	        try {
-	            driver.findElement(webElement);
-	            return true;
-	        } catch (Exception e) {
-	            return false;
-	        }
 
-	    }
+	public static void assertE() {
+
+	}
+
+	/*
+	 * 判断元素是否存在
+	 * 
+	 */
+	public static boolean ElementExist(WebDriver driver, By webElement) {
+		logger.info("判断元素【" + webElement + "】是否存在");
+		try {
+			driver.findElement(webElement);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
 }
